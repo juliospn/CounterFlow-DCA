85302cff5920c46362538763ed7273bf5c4a3807
juliospn <juliospn@gmail.com>
2023-04-16 14:21:16 -0300

    updating files

--- js/lightmode.js
+++ js/lightmode.js
@@ -4,21 +4,29 @@ const lightModeButton = document.querySelector("#light-mode-button");
 
 let isDarkMode = false;
 
-function setColors(isDark) {
+function setColors(isDark, sections) {
   if (isDark) {
     mainElement.style.backgroundColor = "#171717";
     mainElement.style.color = "#f5f5f5";
-    sectionElements.forEach((section) => {
+    sections.forEach((section) => {
       section.style.backgroundColor = "#262626";
       section.style.color = "#f5f5f5";
+      const h2Elements = section.querySelectorAll("h2");
+      h2Elements.forEach((h2) => {
+        h2.style.color = "#ddd";
+      });
     });
     lightModeButton.textContent = "☀ | Light Mode";
   } else {
     mainElement.style.backgroundColor = "#f5f5f5";
     mainElement.style.color = "#171717";
-    sectionElements.forEach((section) => {
+    sections.forEach((section) => {
       section.style.backgroundColor = "#e6e6e6";
       section.style.color = "#171717";
+      const h2Elements = section.querySelectorAll("h2");
+      h2Elements.forEach((h2) => {
+        h2.style.color = "#171717";
+      });
     });
     lightModeButton.textContent = "☾ | Dark Mode";
   }
@@ -26,7 +34,8 @@ function setColors(isDark) {
 
 function toggleDarkMode() {
   isDarkMode = !isDarkMode;
-  setColors(isDarkMode);
+  const sectionsToChange = mainElement.querySelectorAll("section:nth-of-type(n+2):not(:last-child)");
+  setColors(isDarkMode, sectionsToChange);
 }
 
 lightModeButton.addEventListener("click", toggleDarkMode);
