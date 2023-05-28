07b32e1f6c737515aeee33797e7f8cbb4dae442d
juliospn <juliospn@gmail.com>
2023-05-28 18:53:32 -0300

    updating scrollbar

--- js/lightmode.js
+++ js/lightmode.js
@@ -1,41 +1,34 @@
 const mainElement = document.querySelector("main.conteudo");
-const sectionElements = mainElement.querySelectorAll("section");
+const headerElement = document.querySelector("header");
+const sectionElements = document.querySelectorAll("section");
 const lightModeButton = document.querySelector("#light-mode-button");
 
 let isDarkMode = false;
 
-function setColors(isDark, sections) {
-  if (isDark) {
-    mainElement.style.backgroundColor = "#171717";
-    mainElement.style.color = "#f5f5f5";
-    sections.forEach((section) => {
-      section.style.backgroundColor = "#262626";
-      section.style.color = "#f5f5f5";
-      const h2Elements = section.querySelectorAll("h2");
-      h2Elements.forEach((h2) => {
-        h2.style.color = "#ddd";
-      });
+function setColors(isDark, elements) {
+  const backgroundColor = isDark ? "#171717" : "#f5f5f5";
+  const textColor = isDark ? "#f5f5f5" : "#171717";
+
+  elements.forEach((element) => {
+    element.style.backgroundColor = backgroundColor;
+    element.style.color = textColor;
+
+    const h2Elements = element.querySelectorAll("h2");
+    h2Elements.forEach((h2) => {
+      h2.style.color = textColor;
     });
+  });
+
+  if (isDark) {
     lightModeButton.textContent = "☀ | Light Mode";
   } else {
-    mainElement.style.backgroundColor = "#f5f5f5";
-    mainElement.style.color = "#171717";
-    sections.forEach((section) => {
-      section.style.backgroundColor = "#e6e6e6";
-      section.style.color = "#171717";
-      const h2Elements = section.querySelectorAll("h2");
-      h2Elements.forEach((h2) => {
-        h2.style.color = "#171717";
-      });
-    });
     lightModeButton.textContent = "☾ | Dark Mode";
   }
 }
 
 function toggleDarkMode() {
   isDarkMode = !isDarkMode;
-  const sectionsToChange = mainElement.querySelectorAll("section:nth-of-type(n+2):not(:last-child)");
-  setColors(isDarkMode, sectionsToChange);
+  setColors(isDarkMode, [mainElement, headerElement, ...sectionElements]);
 }
 
 lightModeButton.addEventListener("click", toggleDarkMode);
